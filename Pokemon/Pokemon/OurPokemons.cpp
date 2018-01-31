00606a0c9652a5016c691db683c65dbe40e30ced
John Whitehead (whiteh75) <whiteh75@coventry.ac.uk>
2018-01-31 12:48:15 +0000

    added new pokemon

--- Pokemon/Pokemon/OurPokemons.cpp
+++ Pokemon/Pokemon/OurPokemons.cpp
@@ -23,3 +23,21 @@ Pokemon Zizi{ "Zizi", 1, 0, 20, 80, 50, 50, 80,
 	Ability("Nut throw", 17, 0),
 	Ability("Bite", 27, 20),
 	Ability("Hurricane", 55, 50) };
+
+Pokemon Pyro{ "Pyro", 1, 0, 18, 70, 80, 80, 90,
+	"water", "fire", "Pictures/pyro.txt",
+	Ability("Slash", 10, 0),
+	Ability("Ember", 30, 20),
+	Ability("Flameball", 60, 50) };
+
+Pokemon Lemongrass{ "Lemongrass", 1, 0, 17, 65, 75, 75, 95,
+	"fire", "grass", "Pictures/lemongrass.txt",
+	Ability("Scratch", 10, 0),
+	Ability("Tail whip", 25, 10),
+	Ability("Leaf bomb", 55, 45) };
+
+Pokemon Thundershock{ "Thundershock", 1, 0, 20, 75, 80, 80, 90,
+	"earth", "electric", "Pictures/Thundershock.txt",
+	Ability("Claw", 10, 0),
+	Ability("Bolt", 30, 0),
+	Ability("Lightiningblast", 60, 50) };
