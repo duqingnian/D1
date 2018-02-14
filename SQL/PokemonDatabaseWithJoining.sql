25394d97390420284a48f1f24f59fb53525765dc
Krishna Thanki (thankik) <thankik@coventry.ac.uk>
2018-02-14 12:48:49 +0000

    Update PokemonDatabaseWithJoining.sql

--- SQL/PokemonDatabaseWithJoining.sql
+++ SQL/PokemonDatabaseWithJoining.sql
@@ -1,8 +1,6 @@
--- To run this file after saving, use the'@' symbol followed by the location of the file (and file name) in your area...
+-- \\coventry\csv\Students\Personal\k\thankik\Yr1\ALL\SQL\PokemonCTVersion2
 
--- Eg:             @\\coventry\csv\Students\Personal\k\thankik\Yr1\ALL\SQL\Pokemon
-
--- THE TABLE MUST BE DROPPED BEFORE BEING CREATED
+-- DROP THE TABLE BEFORE CREATING IT
 
 drop table Type;
 drop table PokemonTable;
@@ -15,14 +13,10 @@ drop table Power;
 -- CREATE THE TABLE
 
 create table Type (
-   TypeID int not null,
+   TypeID int not null,          -- needs to be not null 
    Type varchar(25),
    Weakness varchar(25)
 );
-
---- "NOT NULL" specifies that there must be a value in the column of the table - it cannot be empty.
---- "VARCHAR(25)" means that the character length in the specific column of the table is a maximum of 25 characters.
---- "DESCRIBE TYPE" will specify the table type being created (varchar, not null etc).
 
 Describe Type;   
 insert into Type values (1, 'EARTH', 'WATER');
@@ -30,8 +24,6 @@ insert into Type values (2, 'GRASS', 'FIRE');
 insert into Type values (3, 'FIRE', 'WATER');
 insert into Type values (4, 'ELECTRIC', 'EARTH');
 insert into Type values (5, 'WIND', 'WATER');
-
---- "SELECT * FROM TYPE" will throw the table 'Type' and its contents onto the terminal.
 
 select * from Type;
 
@@ -44,8 +36,8 @@ select * from Type;
 -- CREATE THE TABLE
 
 create table PokemonTable (
-   PokemonID int not null,
-   TypeID int not null, 
+   PokemonID int not null,          -- needs to be not null
+   TypeID int not null,             -- needs to be not null 
    Name varchar(25),
    PowerID int not null
 );
@@ -69,8 +61,8 @@ select * from PokemonTable;
 -- CREATE THE TABLE
 
 create table AbilityIdent (
-   PokemonID int not null,
-   AbilityID int not null 
+   PokemonID int not null,          -- needs to be not null
+   AbilityID int not null          -- needs to be not null 
 );
 
 Describe AbilityIdent;
@@ -91,10 +83,10 @@ select * from AbilityIdent;
 -- CREATE THE TABLE
 
 create table Ability (
-   AbilityID int not null,
+   AbilityID int not null,          -- needs to be not null
    Ability_Name varchar(25),
-   Damage int not null,
-   Mana int not null
+   Damage int,             -- needs to be not null
+   Mana int
 );
 
 Describe Ability;
@@ -123,19 +115,17 @@ select * from Ability;
 -- CREATE THE TABLE
 
 create table Power (
-   PowerID int not null,
-   Lvl int not null,
-   HP int not null,                 
-   MaxHP int not null,           
-   XP int not null,                 
+   PowerID int not null,            -- needs to be not null
+   Lvl int     not null,            -- needs to be not null
+   HP int      not null,                 
+   MaxHP int   not null,           
+   XP int      not null,                 
    Agility int not null,            
    Strength int not null,           
    MaxMana int not null            
 );
 
 Describe Power;
-
---- EVERYTHING BELOW HAS BEEN WRITTEN AS IF THERE ARE 20 LEVELS OF EACH POKEMON - THIS CAN BE CHANGED IF NEEDED
 
 ---CATTER---
 
@@ -188,15 +178,15 @@ insert into Power values (2, 20, 98, 98, 100, 95, 94, 97);
 ---PYRO---
 
 insert into Power values (3, 1, 80, 80, 5, 70, 18, 90);
-insert into Power values (3, 2, 83, 83, 10, 72, 21, 90);
-insert into Power values (3, 3, 85, 85, 15, 73, 25, 90);
-insert into Power values (3, 4, 87, 87, 20, 74, 30, 92);
-insert into Power values (3, 5, 89, 89, 25, 75, 35, 90);
+insert into Power values (3, 2, 83, 83, 10, 74, 21, 90);
+insert into Power values (3, 3, 85, 85, 15, 79, 25, 90);
+insert into Power values (3, 4, 87, 87, 20, 70, 30, 90);
+insert into Power values (3, 5, 89, 89, 25, 74, 35, 90);
 insert into Power values (3, 6, 89, 89, 30, 77, 40, 90);
 insert into Power values (3, 7, 90, 90, 35, 79, 46, 91);
 insert into Power values (3, 8, 90, 90, 40, 81, 50, 91);
-insert into Power values (3, 9, 91, 91, 45, 83, 55, 90);
-insert into Power values (3, 10, 91, 91, 50, 85, 59, 90);
+insert into Power values (3, 9, 91, 91, 45, 83, 55, 91);
+insert into Power values (3, 10, 91, 91, 50, 85, 59, 91);
 insert into Power values (3, 11, 92, 92, 55, 87, 63, 91);
 insert into Power values (3, 12, 93, 93, 60, 89, 73, 91);
 insert into Power values (3, 13, 93, 93, 65, 90, 77, 92);
@@ -212,48 +202,47 @@ insert into Power values (3, 20, 98, 98, 100, 96, 94, 95);
 
 insert into Power values (4, 1, 80, 80, 5, 75, 20, 90);
 insert into Power values (4, 2, 83, 83, 10, 76, 24, 90);
-insert into Power values (4, 3, 86, 86, 15, 78, 29, 92);
-insert into Power values (4, 4, 88, 88, 20, 79, 34, 94);
-insert into Power values (4, 5, 89, 89, 25, 80, 37, 90);
-insert into Power values (4, 6, 90, 90, 30, 81, 40, 90);
-insert into Power values (4, 7, 91, 91, 35, 82, 46, 91);
-insert into Power values (4, 8, 91, 91, 40, 83, 51, 91);
-insert into Power values (4, 9, 91, 91, 45, 85, 57, 90);
-insert into Power values (4, 10, 91, 91, 50, 87, 63, 90);
-insert into Power values (4, 11, 92, 92, 55, 89, 67, 91);
-insert into Power values (4, 12, 93, 93, 60, 90, 70, 91);
-insert into Power values (4, 13, 94, 94, 65, 91, 73, 92);
-insert into Power values (4, 14, 94, 94, 70, 91, 76, 92);
-insert into Power values (4, 15, 94, 94, 75, 91, 79, 92);
-insert into Power values (4, 16, 95, 95, 80, 92, 82, 92);
-insert into Power values (4, 17, 96, 95, 85, 93, 84, 92);
-insert into Power values (4, 18, 96, 96, 90, 94, 87, 92);
-insert into Power values (4, 19, 97, 97, 95, 95, 94, 93);
-insert into Power values (4, 20, 98, 98, 100, 96, 95, 94);
-
+insert into Power values (4, 3, 86, 86, 15, 77, 29, 91);
+insert into Power values (4, 4, 88, 88, 20, 78, 34, 91);
+insert into Power values (4, 5, 89, 89, 25, 79, 37, 92);
+insert into Power values (4, 6, 90, 90, 30, 80, 40, 92);
+insert into Power values (4, 7, 91, 91, 35, 82, 46, 93);
+insert into Power values (4, 8, 91, 91, 40, 83, 51, 93);
+insert into Power values (4, 9, 91, 91, 45, 85, 57, 94);
+insert into Power values (4, 10, 91, 91, 50, 87, 63, 94);
+insert into Power values (4, 11, 92, 92, 55, 89, 67, 95);
+insert into Power values (4, 12, 93, 93, 60, 90, 70, 95);
+insert into Power values (4, 13, 94, 94, 65, 91, 73, 96);
+insert into Power values (4, 14, 94, 94, 70, 91, 76, 96);
+insert into Power values (4, 15, 94, 94, 75, 91, 79, 97);
+insert into Power values (4, 16, 95, 95, 80, 92, 82, 97);
+insert into Power values (4, 17, 96, 95, 85, 93, 84, 98);
+insert into Power values (4, 18, 96, 96, 90, 94, 87, 98);
+insert into Power values (4, 19, 97, 97, 95, 95, 94, 99);
+insert into Power values (4, 20, 98, 98, 100, 96, 95, 99);
 
 ---ZIZI---
 
 insert into Power values (5, 1, 50, 50, 5, 80, 20, 80);
 insert into Power values (5, 2, 54, 54, 10, 86, 26, 80);
-insert into Power values (5, 3, 59, 59, 15, 87, 30, 82);
-insert into Power values (5, 4, 63, 63, 20, 88, 35, 84);
-insert into Power values (5, 5, 67, 67, 25, 89, 40, 80);
-insert into Power values (5, 6, 70, 70, 30, 90, 45, 80);
-insert into Power values (5, 7, 76, 76, 35, 91, 47, 82);
-insert into Power values (5, 8, 79, 79, 40, 91, 52, 82);
-insert into Power values (5, 9, 83, 83, 45, 92, 55, 80);
-insert into Power values (5, 10, 85, 85, 50, 92, 58, 80);
-insert into Power values (5, 11, 87, 87, 55, 93, 60, 82);
-insert into Power values (5, 12, 88, 88, 60, 94, 64, 84);
-insert into Power values (5, 13, 90, 90, 65, 94, 68, 85);
-insert into Power values (5, 14, 91, 91, 70, 94, 71, 85);
-insert into Power values (5, 15, 92, 92, 75, 94, 74, 85);
-insert into Power values (5, 16, 93, 93, 80, 95, 77, 85);
-insert into Power values (5, 17, 94, 94, 85, 96, 79, 85);
-insert into Power values (5, 18, 95, 95, 90, 97, 83, 85);
-insert into Power values (5, 19, 96, 96, 95, 98, 95, 90);
-insert into Power values (5, 20, 97, 97, 100, 99, 96, 91)
+insert into Power values (5, 3, 59, 59, 15, 87, 30, 81);
+insert into Power values (5, 4, 63, 63, 20, 88, 35, 82);
+insert into Power values (5, 5, 67, 67, 25, 89, 40, 83);
+insert into Power values (5, 6, 70, 70, 30, 90, 45, 84);
+insert into Power values (5, 7, 76, 76, 35, 91, 47, 85);
+insert into Power values (5, 8, 79, 79, 40, 91, 52, 86);
+insert into Power values (5, 9, 83, 83, 45, 92, 55, 87);
+insert into Power values (5, 10, 85, 85, 50, 92, 58, 88);
+insert into Power values (5, 11, 87, 87, 55, 93, 60, 89);
+insert into Power values (5, 12, 88, 88, 60, 94, 64, 90);
+insert into Power values (5, 13, 90, 90, 65, 94, 68, 91);
+insert into Power values (5, 14, 91, 91, 70, 94, 71, 92);
+insert into Power values (5, 15, 92, 92, 75, 94, 74, 93);
+insert into Power values (5, 16, 93, 93, 80, 95, 77, 94);
+insert into Power values (5, 17, 94, 94, 85, 96, 79, 95);
+insert into Power values (5, 18, 95, 95, 90, 97, 83, 96);
+insert into Power values (5, 19, 96, 96, 95, 98, 95, 97);
+insert into Power values (5, 20, 97, 97, 100, 99, 96, 98)
 
 
 select * from Power;
