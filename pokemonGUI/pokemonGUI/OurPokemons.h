6f2c67f64681ee0ec878fa6fa5500a1c152dcdab
John Whitehead (whiteh75) <whiteh75@coventry.ac.uk>
2018-03-02 21:26:49 +0000

    Update OurPokemons.h

--- pokemonGUI/pokemonGUI/OurPokemons.h
+++ pokemonGUI/pokemonGUI/OurPokemons.h
@@ -7,7 +7,14 @@ extern Pokemon Zizi;
 extern Pokemon Pyro;
 extern Pokemon Lemongrass;
 extern Pokemon Thundershock;
+extern Pokemon Wail;
+extern Pokemon Blaze;
+extern Pokemon Flyff;
+extern Pokemon Jolt;
+extern Pokemon Treehouse;
+extern Pokemon Deeria;
+extern Pokemon Hydros;
+extern Pokemon Flop;
 
-//extern array<Pokemon, 5> pokemonArray = { Catter, Zizi, Pyro, Lemongrass, Thundershock };
-
+//extern array<Pokemon, 13> pokemonArray = { Catter, Zizi, Pyro, Lemongrass, Thundershock, Wail, Blaze, Flyff, Jolt, Treehouse, Deeria, Hydros, Flop };
 
